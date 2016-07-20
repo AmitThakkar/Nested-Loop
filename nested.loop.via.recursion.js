b711e52f11b2f241dcaf85d2e38e1b7fc81ec7a4
AmitThakkar <vigildbest@gmail.com>
2016-07-20 10:49:12 +0530

    Adding sample code for loop on an array..

--- nested.loop.via.recursion.js
+++ nested.loop.via.recursion.js
@@ -1,17 +1,30 @@
 /**
  * Created by amitthakkar on 19/07/16.
  */
-const nestedCount = 2;
+const nestedCount = 3;
 const loopLength = 5;
 const tempArray = [];
-let loop = (start, index) => {
+let loopOnLength = (start, index) => {
     if (index == nestedCount) {
         console.log(tempArray);
     } else {
         for (let i = start; i < loopLength; i++) {
             tempArray[index] = i;
-            loop(i + 1, index + 1)
+            loopOnLength(i + 1, index + 1)
         }
     }
 };
-loop(0, 0);
+// loopOnLength(0, 0);
+
+const loopArray = [1, 2, 3, 4, 5];
+let loopOnArray = (start, index) => {
+    if (index == nestedCount) {
+        console.log(tempArray);
+    } else {
+        for (let i = start; i < loopArray.length; i++) {
+            tempArray[index] = loopArray[i];
+            loopOnArray(i + 1, index + 1)
+        }
+    }
+};
+loopOnArray(0, 0);
